280123cd640799d28af5af57d3af49ecb0472a5d
Ginny Mae <ginny@mae.lol>
2025-09-02 03:36:36 -0700

    add status mapping and support for self-reply posts. also adds a working function to upload media for use later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import time
+# I'd use ulid but it'd be silly to require that dependency just for dry runs :3
 import uuid
 import webbrowser
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -12,9 +13,11 @@ INSTANCE_BASE_URL = 'https://transister.social/'
 AUTHORIZATION_URL = INSTANCE_BASE_URL + 'oauth/authorize'
 TOKEN_URL = INSTANCE_BASE_URL + 'oauth/token'
 STATUS_URL = INSTANCE_BASE_URL + 'api/v1/statuses'
+MEDIA_URL = INSTANCE_BASE_URL + 'api/v1/media'
 
 CREDENTIALS_FILE = 'credentials.json'
 APPLICATION_FILE = 'application.json'
+STATUS_MAPPING_FILE = 'status_map.json'
 # These will be read and set from APPLICATION_FILE
 CLIENT_ID = ''
 CLIENT_SECRET = ''
@@ -70,6 +73,26 @@ def get_token(auth_code):
     response.raise_for_status()
     return response.json()
 
+def upload_media(creds, file_path, description):
+    access_token = creds.get('access_token')
+    if not access_token:
+        raise ValueError('No access token found in credentials.')
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+    }
+    payload = {
+        'description': description
+    }
+    files = {'file': open(file_path, 'rb')}
+    response = requests.post(
+        MEDIA_URL,
+        headers=headers,
+        data=payload,
+        files=files)
+    response.raise_for_status()
+    print('Uploaded media:', response.json())
+    return response.json()
+
 def post_status_dryrun(creds, payload):
     print('DRY RUN - making some stuff up')
     fake_id = uuid.uuid4()
@@ -77,6 +100,25 @@ def post_status_dryrun(creds, payload):
         'uri': INSTANCE_BASE_URL + 'users/gintoxicating/statuses/' + str(fake_id),
         'url': INSTANCE_BASE_URL + '@gintoxicating/statuses/' + str(fake_id),
         'id': str(fake_id)}
+
+def get_existing_status_mapping(sharkey_id):
+    if os.path.exists(STATUS_MAPPING_FILE):
+        with open(STATUS_MAPPING_FILE, 'r') as f:
+            mapping = json.load(f)
+            return mapping.get(sharkey_id)
+    return None
+
+def save_status_mapping(sharkey_id, opensocial_id, opensocial_url):
+    print('Saving mapping:', sharkey_id, '->', opensocial_id)
+    mapping = {}
+    if os.path.exists(STATUS_MAPPING_FILE):
+        with open(STATUS_MAPPING_FILE, 'r') as f:
+            mapping = json.load(f)
+    if sharkey_id in mapping:
+        print('!@@@!@@*** Warning: overwriting existing mapping for', sharkey_id)
+    mapping[sharkey_id] = {'opensocial_id': opensocial_id, 'opensocial_url': opensocial_url}
+    with open(STATUS_MAPPING_FILE, 'w') as f:
+        json.dump(mapping, f, indent=2)
 
 def post_status(creds, payload):
     access_token = creds.get('access_token')
@@ -97,12 +139,12 @@ def post_status(creds, payload):
 def authorize():
     creds = load_credentials()
     if creds:
-        print('Credentials loaded:', creds)
+        print('Credentials loaded')
     else:
         auth_code = get_auth_code()
         token_data = get_token(auth_code)
         save_credentials(token_data)
-        print('Credentials saved:', token_data)
+        print('Credentials saved')
         creds = load_credentials()
     return creds
 
@@ -149,8 +191,18 @@ def process_note(creds, note):
         this_note_sucks_sorry(note, 'has specified-user visibility and i don\'t want to implement that')
         return
 
-    # TODO: check if this post has already been posted (it's in our post mapping file) and skip if so
-    # TODO: check the replyId and get the new ID for the reply or skip if we can't do that
+    if get_existing_status_mapping(note.get('id')):
+        this_note_sucks_sorry(note, 'already has a mapping, so it was already posted')
+        return
+
+    if note.get('replyId'):
+        note_map = get_existing_status_mapping(note.get('replyId'))
+        if note_map:
+            payload['in_reply_to_id'] = note_map.get('opensocial_id')
+        else:
+            this_note_sucks_sorry(note, 'is a reply but the parent note was not found, so skipping')
+            return
+
     print('-----')
     print('cool note: ', note)
     print(payload)
@@ -159,11 +211,12 @@ def process_note(creds, note):
     print('   ID: ', response.get('id'))
     print('   URI: ', response.get('uri'))
     print('raw response:', response)
-    # TODO: save id mapping to file
+    save_status_mapping(note.get('id'), response.get('id'), response.get('url'))
     print('  sleeping', DELAY_BETWEEN_REQUESTS, 'seconds to respect rate limits')
     time.sleep(DELAY_BETWEEN_REQUESTS)
     print('-----')
     return
+
 
 def set_app_config():
     if os.path.exists(APPLICATION_FILE):
@@ -177,7 +230,6 @@ def set_app_config():
 
 def print_notes():
     set_app_config()
-    print('Using CLIENT_ID:', CLIENT_ID)
     creds = authorize()
     if not creds:
         print('No credentials found. Please authorize first.')
@@ -193,7 +245,6 @@ def print_notes():
     print('  **! Skipped', SKIPPED, 'notes!')
 
 def main():
-    # creds = authorize()
     print_notes()
 
 if __name__ == '__main__':
